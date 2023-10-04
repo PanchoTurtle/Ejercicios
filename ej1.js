6195266ccf8fec4b50cf276d239158c82eae4662
PanchoTurtle <omarbracamontes102@gmail.com>
2023-10-03 20:03:14 -0700

    Se agrega el proyecto

---
+++ ej1.js
@@ -0,0 +1,34 @@
+//1
+
+/*let numeros = [1,12,3,6,7,8,2,32,13];
+const mayores = numeros.filter((numeros)=> numeros >=5);
+mayores.forEach((mayores)=> console.log(mayores));*/
+
+//2
+
+/*let frutas = ["naranja","platano","Manzana","fresa","kiwi"];
+let sacar = frutas.slice(2, 4);
+sacar.forEach((sacar)=> console.log(sacar));*/
+
+//3
+
+let numeros = [10, 4, 9];
+numeros.map(function (num, index) {
+    console.log("valor: "+ (num + 10) + " indice: " + index);
+  
+});
+
+
+
+
+//4
+
+/*let array1 = ["H","I","J"];
+let array2 = ["K","L","M"];
+let array3 = array1.concat(array2);
+console.log(array3);*/
+
+//5
+
+/*let paises = ["Mexico","USA","Francia"];
+console.log(paises.includes("Francia"));*/
